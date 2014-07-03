20d65c392c68c25fba8f491bc0e41469f91a91db
Matthew Dahl <daftdahl@gmail.com>
2014-07-03 12:39:56 -0700

    Reworking template AND adding javascript

--- routes.py
+++ routes.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/cit-142')
+@app.route('/homework')
 def homework():
+    return render_template('cit-142.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/cit-142')
+def cit142():
     return render_template('cit-142.html')
 
 if __name__ == '__main__':
